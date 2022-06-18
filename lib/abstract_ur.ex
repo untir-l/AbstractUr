a883de1d6555fa4828ea00b8c10ca4c1640e82e3
untir_l <87096069+untir-l@users.noreply.github.com>
2022-06-18 23:35:29 +0530

    Initial commit

---
+++ lib/abstract_ur.ex
@@ -0,0 +1,18 @@
+defmodule AbstractUr do
+  @moduledoc """
+  Documentation for `AbstractUr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AbstractUr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/abstract_ur_test.exs
@@ -0,0 +1,8 @@
+defmodule AbstractUrTest do
+  use ExUnit.Case
+  doctest AbstractUr
+
+  test "greets the world" do
+    assert AbstractUr.hello() == :world
+  end
+end
